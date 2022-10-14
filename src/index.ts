1ad5e7c308fdc0b66099cf3f0aee901713eee6fa
coderspinoza <hara0115@gmail.com>
2022-10-14 18:39:08 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export const add = (a: number, b: number): number => a + b;
+import { ethers, providers } from 'ethers';
+
+export const fetchTokens = async (
+  provider: providers.AlchemyWebSocketProvider,
+  contractAddress: string,
+  address: string,
+) => {
+  const abi = ['function balanceOf(address owner) view returns (uint256)'];
+
+  const contract = new ethers.Contract(contractAddress, abi, provider);
+  const res = await contract.balanceOf(address);
+  return res;
+};
---
+++ test/ethers.test.ts
@@ -0,0 +1,3 @@
+describe(`NFT Query`, () => {
+  it(``)
+});
